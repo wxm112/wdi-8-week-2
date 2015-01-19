eb883aa927a14f5e1ff12b3f8e0c073522e8fee7
王雪梅 <wxm112@me.com>
2015-01-19 18:01:17 +1100

    All pages are done

--- pair/main.rb
+++ pair/main.rb
@@ -5,3 +5,28 @@ get '/' do
 
   erb :mainpage
 end
+
+get '/pass' do
+
+  erb :pass
+end
+
+get '/refactor' do
+
+  erb :refactor
+end
+
+get '/write_code' do
+
+  erb :write_code
+end
+
+get '/write_test' do
+
+  erb :write_test
+end
+
+get '/do_refactor' do
+
+  erb :do_refactor
+end
