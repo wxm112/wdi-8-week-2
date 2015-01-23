8cefaf5cf3cd8dc767ea7cca2eff1c01d7f8900f
王雪梅 <wxm112@me.com>
2015-01-23 17:22:18 +1100

    2nd version of MTA web

--- mta/main.rb
+++ mta/main.rb
@@ -2,9 +2,9 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
 
-LINES = { 'line_n' => ['times square', '34th', '28th', '23rd','union square', '8th'], 
-        'line_l' => ['8th', '6th', 'union square', '3rd', '1st'], 
-        'line_6' => ['grand central', '33rd', '28th', '23rd', 'union square', 'astor place'] }
+LINES = { 'line n' => ['times square', '34th', '28th', '23rd','union square', '8th'], 
+        'line l' => ['8th', '6th', 'union square', '3rd', '1st'], 
+        'line 6' => ['grand central', '33rd', '28th', '23rd', 'union square', 'astor place'] }
 
 
 get '/' do
@@ -14,39 +14,33 @@ end
 
 
 get '/result' do 
-    @start_line = params['start_line']
-    @start_station = params[:start_station]
-    @destination_line = params[:destination_line]
-    @destination_station = params[:destination_station]
+    
+    @start_point = params[:start_point]
+    @destination_point = params[:destination_point]
+    @start_line = @start_point.split(':')[0]
+    @start_station = @start_point.split(':')[1].strip
+    @destination_line = @destination_point.split(':')[0]
+    @destination_station = @destination_point.split(':')[1].strip
+    
     if @start_line == @destination_line
         line = LINES[@start_line.downcase]
-
-    if line.nil?
-      # puts "There is no such line: #{start_line}"
-      @stations = nil
-    else
-      s_index = line.index(@start_station)
-      d_index = line.index(@destination_station)
-      if s_index < d_index
+        s_index = line.index(@start_station)
+        d_index = line.index(@destination_station)
+        if s_index < d_index
         @stations = line[s_index..d_index]
         @stations_num = @stations.count
       else
         @stations = line[d_index..s_index].reverse
         @stations_num = @stations.count
       end
-    end
     # If the start and ending stations aren't in the same line.
   else
     s_line = LINES[@start_line.downcase]
     d_line = LINES[@destination_line.downcase]
-    if s_line.nil? || d_line.nil?
-      # puts "There is no such line: #{start[0]}"
-      @stations = nil
-    else
-      s_index = s_line.index(@start_station)
-      u_s_index = s_line.index('union square')
-      u_d_index = d_line.index('union square')
-      d_index = d_line.index(@destination_station)
+    s_index = s_line.index(@start_station)
+    u_s_index = s_line.index('union square')
+    u_d_index = d_line.index('union square')
+    d_index = d_line.index(@destination_station)
       if s_index < u_s_index
         f_stations = s_line[s_index..u_s_index]
       else
@@ -59,7 +53,6 @@ get '/result' do
       end
       @stations = (f_stations - ['union square'] + l_stations)
       @stations_num = @stations.count
-    end
   end
     erb :result
 end
