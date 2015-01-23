a82276d7d7792c331e65f8edecd3adda8d6431f8
王雪梅 <wxm112@me.com>
2015-01-23 14:28:42 +1100

    added receipts folder

---
+++ receipts/main.rb
@@ -0,0 +1,33 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'pry'
+
+get '/' do 
+  erb :form   
+end
+
+
+post '/' do 
+    db = File.open('receipts.csv', 'a+')
+    db.puts params.values.join(',') # where to use #{}
+    db.close
+    erb :thanks
+end
+
+
+get '/all' do
+    db = File.open('receipts.csv', 'r')
+    @receipts = []
+
+    db.each do |line|
+        info = line.split(',')
+        receipt = {
+            :client => info[0],
+            :serivce => info[1],
+            :cost => info[2],
+        }
+        @receipts << receipt
+        end
+    db.close
+    erb :all
+end
